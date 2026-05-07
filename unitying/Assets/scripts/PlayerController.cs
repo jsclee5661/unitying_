a8cd00947b18b578697136a4386caa529a1196fb
jsclee5661 <jsclee5661@gmail.com>
2026-05-07 10:43:45 +0900

    Add Player movement system

---
+++ unitying/Assets/scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        input = input.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + input * moveSpeed * Time.fixedDeltaTime);
+    }
+}
